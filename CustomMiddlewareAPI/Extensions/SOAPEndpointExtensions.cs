da2733a266fc11155739235b0fb13cf7b24c2388
Valteci Oliveira <valteci@gmail.com>
2020-07-13 14:11:44 -0300

    Initial Commit

---
+++ CustomMiddlewareAPI/Extensions/SOAPEndpointExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+
+using CustomMiddleware;
+using System.ServiceModel.Channels;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class SOAPEndpointExtensions
+    {
+        public static IApplicationBuilder UseSOAPEndpoint<T>(this IApplicationBuilder builder, string path, Binding binding)
+        {
+            var encoder = binding.CreateBindingElements().Find<MessageEncodingBindingElement>()?.CreateMessageEncoderFactory().Encoder;
+            return builder.UseMiddleware<SOAPEndpointMiddleware>(typeof(T), path, encoder);
+        }
+    }
+}
